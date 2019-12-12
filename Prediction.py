702b7ea1b3ccce78d08b614ae412545c4916c118
Umar Ahsan <umar1100@gmail.com>
2019-12-11 21:41:45 -0700

    Reading and Uploading the file

--- Prediction.py
+++ Prediction.py
@@ -1,2 +1,10 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+import sklearn.preprocessing
+
+stock_data_bac = pd.read_csv('BAC.csv')
+stock_data_bac.info()
+stock_data_bac.head()
+
